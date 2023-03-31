067d55abd5622b2f80a3cff2053e19535d5d3801
P.rometheus <112591537+root-Prometheus@users.noreply.github.com>
2023-03-31 06:56:02 +0300

    Create print_bits.c

---
+++ print_bits.c
@@ -0,0 +1,11 @@
+void	print_bits(unsigned char octet)
+{
+    int i = 8;
+    while(i--)
+    {
+        if ((octet >> i  & 1) == 1)
+            write(1,"1",1);
+        else
+            write(1,"0",1);
+    }
+}
