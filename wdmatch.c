432f78b91537bae068ae09de2833447fb730581b
P.rometheus <112591537+root-Prometheus@users.noreply.github.com>
2023-03-31 06:07:47 +0300

    Create wdmatch.c

---
+++ wdmatch.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+
+int check(char *str,char word)
+{
+    int x = 0;
+
+    while(str[x] != '\0')
+    {
+        if(str[x] == word)
+            return 1;
+        x++;
+    }
+    return 0;
+}
+int main(int argc, char **argv)
+{
+    int i = 0;
+    int x = 0;
+    char word[999];
+    if (argc == 3)
+    {
+        while(argv[1][i] != '\0')
+        {
+            if (check(&argv[2][i],argv[1][i]) == 1)
+            {
+                    word[x] = argv[1][i];
+                    x++;
+            }
+            i++;
+        }
+        write(1,word,x);
+        write(1,"\n",1);
+    }
+    else
+        write(1,"\n",1);
+}
