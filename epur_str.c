7a480942f8f136bb8abadeb0b3fba60bd9fc3f3f
P.rometheus <112591537+root-Prometheus@users.noreply.github.com>
2023-03-31 10:59:29 +0300

    Create epur_str.c

---
+++ epur_str.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    int x = 0;
+    if (argc == 2)
+    {
+        if(argv[1][x] ==  '\0')
+            write(1,"\n",1);
+        while(argv[1][x] != '\0')
+        {
+        
+           if (argv[1][x] == ' ' && x != 0)
+           {
+                if (argv[1][x+1] != '\0') 
+                    write(1," ",1);
+                while(argv[1][x] == ' ')
+                    x++;
+           }
+           else if (x == 0)
+           {
+            if (argv[1][0] == ' ')
+                x++;
+            else
+                write(1,&argv[1][x],1);
+           }
+           else{
+            write(1,&argv[1][x],1);
+            x++;
+           }
+            
+        }
+    }
+    else
+        write(1,"\n",1);
+}
